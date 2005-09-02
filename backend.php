16a7aacd5683b55fb0fda8177d3c0da54f551b38
Andrew Dolgov <fox@bah.spb.su>
2005-09-02 16:40:31 +0100

    OPML Export button renamed again, remove Refresh button from prefs

--- backend.php
+++ backend.php
@@ -567,12 +567,8 @@
 				href=\"javascript:readSelectedFeeds()\">Mark as read</a>&nbsp;
 			<a class=\"button\" 
 				href=\"javascript:unreadSelectedFeeds()\">Mark as unread</a>&nbsp;
-			Page: 
-				<a class=\"button\" 
-				href=\"javascript:updateFeedList()\">Refresh</a>&nbsp;
-			OPML: 
-				<a class=\"button\" href=\"opml.php?op=Export\">Export all</a>
-				";
+			All feeds:&nbsp; 
+				<a class=\"button\" href=\"opml.php?op=Export\">Export OPML</a>";
 		
 			}
 
