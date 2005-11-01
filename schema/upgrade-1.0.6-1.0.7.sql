833188150fb05b2a2672673590e592ae3e2b416d
Andrew Dolgov <fox@madoka.spb.ru>
2005-11-01 10:01:03 +0100

    schema/upgrade-1.0.6-1.0.7.sql: fix

--- schema/upgrade-1.0.6-1.0.7.sql
+++ schema/upgrade-1.0.6-1.0.7.sql
@@ -1,6 +1,6 @@
 alter table ttrss_feeds add column last_error text;
 
-update ttrss_feeds set not last_error = '';
+update ttrss_feeds set last_error = '';
 
 alter table ttrss_feeds alter column last_error set not null;
 alter table ttrss_feeds alter column last_error set default '';
