ac6080ece78a53fb27fdd67e939bf46c92e40baf
Andrew Dolgov <fox@bah.spb.su>
2005-11-19 12:31:20 +0100

    remove access_level session var just in case

--- logout.php
+++ logout.php
@@ -3,9 +3,10 @@
 
 	$_SESSION["uid"] = null;
 	$_SESSION["name"] = null;
-	
+	$_SESSION["access_level"] = null;
+
 	session_destroy();
 
-	header("Location: tt-rss.php");
+	header("Location: login.php");
 
 ?>
