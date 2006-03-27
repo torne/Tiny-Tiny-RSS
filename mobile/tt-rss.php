ca35939d207237fb3cf3f8f63fb888d62280f24c
Andrew Dolgov <fox@bah.spb.su>
2006-03-27 03:46:22 +0100

    more mobile work

--- mobile/tt-rss.php
+++ mobile/tt-rss.php
@@ -1,5 +1,6 @@
 <?
 	require_once "../functions.php"; 
+	require_once "functions.php";
 
 	basic_nosid_redirect_check();
 
@@ -10,6 +11,9 @@
 	require_once "../db-prefs.php";
 
 	$link = db_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);	
+
+	login_sequence($link);
+
 ?>
 <html>
 <head>
@@ -17,6 +21,35 @@
 	<link rel="stylesheet" type="text/css" href="mobile.css">
 </head>
 <body>
+
+<table width='640' height='100%'>
+<tr><td class="heading">
+	Your Feeds
+</td>
+<td align='right'>
+	<form method="GET">
+		<select name="go">
+			<option>Feeds</option>
+			<option>Preferences</option>
+			<option disabled>--------------</option>
+			<option disabled>--------------</option>
+			<option>Logout</option>
+		</select>
+		<input type="submit" value="Go">
+	</form>
+</td>
+</tr>
+<td class="content" height='100%' colspan='2' valign='top'>
+<?
+	$go = $_GET["go"];
+
+	if (!$go || $go == "Feeds") {
+		render_feeds_list($link);
+	}
+
+?>
+</td></tr>
+</table>
 
 </body>
 </html>
