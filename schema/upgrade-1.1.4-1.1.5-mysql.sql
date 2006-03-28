3b0f23de404fb1616a80db06b8641d2f44a80291
Andrew Dolgov <fox@madoka.spb.ru>
2006-03-28 04:45:30 +0100

    schema: drop ip_address from ttrss_sessions

--- schema/upgrade-1.1.4-1.1.5-mysql.sql
+++ schema/upgrade-1.1.4-1.1.5-mysql.sql
@@ -5,5 +5,7 @@ update ttrss_feeds set rtl_content = false;
 alter table ttrss_feeds change rtl_content rtl_content bool not null;
 alter table ttrss_feeds alter column rtl_content set default false;
 
+alter table ttrss_sessions drop column ip_address;
+
 update ttrss_version set schema_version = 7;
 
--- schema/upgrade-1.1.4-1.1.5-pgsql.sql
+++ schema/upgrade-1.1.4-1.1.5-pgsql.sql
@@ -7,6 +7,8 @@ update ttrss_feeds set rtl_content = false;
 alter table ttrss_feeds alter column rtl_content set not null;
 alter table ttrss_feeds alter column rtl_content set default false;
 
+alter table ttrss_sessions drop column ip_address;
+
 update ttrss_version set schema_version = 7;
 
 commit;
