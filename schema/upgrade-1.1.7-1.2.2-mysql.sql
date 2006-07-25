52c61bf8666e83f6be708b343e957ba6fb54933b
Andrew Dolgov <fox@madoka.spb.ru>
2006-07-25 09:12:17 +0100

    add schema update scripts for 1.2.2

---
+++ schema/upgrade-1.1.7-1.2.2-mysql.sql
@@ -0,0 +1,4 @@
+insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('MARK_UNREAD_ON_UPDATE', 1, 'false', 'Set articles as unread on update',3);
+
+update ttrss_version set schema_version = 9;
+
