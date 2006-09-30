800a71e0f10799d10271506b289802df11166069
Andrew Dolgov <fox@bah.spb.su>
2006-09-30 17:33:56 +0100

    add graycube theme

--- schema/upgrade-1.2.3-1.2.4-mysql.sql
+++ schema/upgrade-1.2.3-1.2.4-mysql.sql
@@ -6,6 +6,9 @@ delete FROM ttrss_user_prefs WHERE pref_name = 'DISPLAY_FOOTER';
 delete FROM ttrss_prefs WHERE pref_name = 'DISPLAY_HEADER';
 delete FROM ttrss_prefs WHERE pref_name = 'DISPLAY_FOOTER';
 
+insert into ttrss_themes (theme_name, theme_path) 
+	values ('Graycube', 'graycube');
+
 update ttrss_version set schema_version = 11;
 
 commit;
