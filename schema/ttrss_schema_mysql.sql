3fb00ee0a242b4b5109434e43d2d44335968f509
Andrew Dolgov <fox@madoka.spb.ru>
2007-03-06 08:20:43 +0100

    schema: fix typo

--- schema/ttrss_schema_mysql.sql
+++ schema/ttrss_schema_mysql.sql
@@ -265,7 +265,7 @@ insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id,help_
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('CONFIRM_FEED_CATCHUP', 1, 'true', 'Confirm marking feed as read',3);
 
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id,help_text) values('CDM_AUTO_CATCHUP', 1, 'false', 'Mark articles as read automatically',2,
-'This option enables marking articles as read automatically in combined mode while you scroll article list');
+'This option enables marking articles as read automatically in combined mode while you scroll article list.');
 
 create table ttrss_user_prefs (
    owner_uid integer not null,
