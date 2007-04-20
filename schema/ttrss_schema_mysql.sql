e85610c7103434df9689833c0c81ad75a78e45c1
Andrew Dolgov <fox@madoka.spb.ru>
2007-04-20 03:59:27 +0100

    schema: fix version

--- schema/ttrss_schema_mysql.sql
+++ schema/ttrss_schema_mysql.sql
@@ -189,7 +189,7 @@ create table ttrss_tags (id integer primary key auto_increment,
 
 create table ttrss_version (schema_version int not null) TYPE=InnoDB;
 
-insert into ttrss_version values (15);
+insert into ttrss_version values (16);
 
 create table ttrss_prefs_types (id integer not null primary key, 
 	type_name varchar(100) not null) TYPE=InnoDB;
--- schema/ttrss_schema_pgsql.sql
+++ schema/ttrss_schema_pgsql.sql
@@ -173,7 +173,7 @@ create index ttrss_tags_owner_uid_index on ttrss_tags(owner_uid);
 
 create table ttrss_version (schema_version int not null);
 
-insert into ttrss_version values (15);
+insert into ttrss_version values (16);
 
 create table ttrss_prefs_types (id integer not null primary key, 
 	type_name varchar(100) not null);
