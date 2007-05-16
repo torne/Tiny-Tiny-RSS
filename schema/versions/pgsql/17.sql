f335e9659dcf565e2cf1fdc3901255d95541c575
Andrew Dolgov <fox@madoka.spb.ru>
2007-05-16 09:35:56 +0100

    change description of STRIP_UNSAFE_TAGS

--- schema/versions/pgsql/17.sql
+++ schema/versions/pgsql/17.sql
@@ -3,6 +3,6 @@ insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) valu
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id) values('_INFOBOX_DISABLE_OVERLAY', 1, 'false', '', 1);
 
 insert into ttrss_prefs (pref_name,type_id,def_value,short_desc,section_id,help_text) values('STRIP_UNSAFE_TAGS', 1, 'true', 'Strip unsafe tags from articles', 3,
-'This option strips all, but most common HTML tags when reading articles.');
+'Strip all but most common HTML tags when reading articles.');
 
 update ttrss_version set schema_version = 17;
