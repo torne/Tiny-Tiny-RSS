7a64852c8e4136b4eadd0523d0a4386421392b6c
Andrew Dolgov <fox@bah.spb.su>
2007-08-11 03:55:25 +0100

    hide read feeds also hides labels

--- functions.js
+++ functions.js
@@ -1010,6 +1010,7 @@ function hideOrShowFeedsCategory(doc, node, hide, cat_node) {
 			if (node.childNodes[i].style != undefined) {
 
 				var has_unread = (node.childNodes[i].className != "feed" &&
+					node.childNodes[i].className != "label" && 
 					node.childNodes[i].className != "tag");
 	
 	//			debug(node.childNodes[i].id + " --> " + has_unread);
