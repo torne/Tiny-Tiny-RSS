02ceed6e078a541332030adbb50d5fb9cfd093b1
Andrew Dolgov <fox@madoka.spb.ru>
2007-10-24 07:53:29 +0100

    drop tagwall from trunk (mobile)

--- mobile/functions.php
+++ mobile/functions.php
@@ -2,8 +2,6 @@
 	define('MOBILE_FEEDLIST_ENABLE_ICONS', false);
 	define('TTRSS_SESSION_NAME', 'ttrss_m_sid');
 
-	define ('TW_PATH', '../tw/');
-
 	function render_feeds_list($link) {
 
 		$tags = $_GET["tags"];
