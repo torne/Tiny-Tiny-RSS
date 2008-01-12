f6d40ed212f2e767b44aa916ea957d86a4aeba4d
Andrew Dolgov <fox@bah.spb.su>
2008-01-12 13:05:29 +0100

    js: make displayHelpInfobox() globally accessible

--- functions.js
+++ functions.js
@@ -1731,4 +1731,12 @@ function getSelectedArticleIds2() {
 	return ids;
 }
 
+function displayHelpInfobox(topic_id) {
 
+	var url = "backend.php?op=help&tid=" + param_escape(topic_id);
+
+	var w = window.open(url, "ttrss_help", 
+		"status=0,toolbar=0,location=0,width=450,height=500,scrollbars=1,menubar=0");
+
+	return false;
+}
--- prefs.js
+++ prefs.js
@@ -856,18 +856,6 @@ function feedCatEditSave() {
 	return false;
 }
 
-
-
-function displayHelpInfobox(topic_id) {
-
-	var url = "backend.php?op=help&tid=" + param_escape(topic_id);
-
-	var w = window.open(url, "ttrss_help", 
-		"status=0,toolbar=0,location=0,width=450,height=500,scrollbars=1,menubar=0");
-
-	return false;
-}
-
 function labelEditCancel() {
 
 	if (!xmlhttp_ready(xmlhttp)) {
