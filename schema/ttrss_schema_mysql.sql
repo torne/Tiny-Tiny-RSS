f65b8485c6ec27fb9082b46c18132317267f789d
Andrew Dolgov <fox@madoka.spb.ru>
2008-01-14 06:49:00 +0100

    fix schema version

--- schema/ttrss_schema_mysql.sql
+++ schema/ttrss_schema_mysql.sql
@@ -203,7 +203,7 @@ create table ttrss_tags (id integer primary key auto_increment,
 
 create table ttrss_version (schema_version int not null) TYPE=InnoDB;
 
-insert into ttrss_version values (26);
+insert into ttrss_version values (27);
 
 create table ttrss_enclosures (id serial not null primary key,
 	content_url text not null,
