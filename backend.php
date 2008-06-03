3bfc7a2ff1874d5a98a1d9159676ee5bcadc774d
Andrew Dolgov <fox@madoka.spb.ru>
2008-06-03 09:14:07 +0100

    view: output counters only when called in prefetch mode or when csync is enabled

--- backend.php
+++ backend.php
@@ -212,7 +212,7 @@
 				}
 			}
 
-			if ($mode != "prefetch_old" || $csync) {
+			if ($mode == "prefetch" || $csync) {
 				print "<counters>";
 				getAllCounters($link, $omode);
 				print "</counters>";
