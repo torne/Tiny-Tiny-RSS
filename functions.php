d2b1c99be6b27b9bb079cc9843003235f04f2863
Andrew Dolgov <fox@bah.spb.su>
2009-01-19 04:35:33 +0100

    getLabelCounters: fix for read labels

--- functions.php
+++ functions.php
@@ -2641,10 +2641,9 @@
 					LEFT JOIN ttrss_user_labels2 ON (label_id = ttrss_labels2.id) 
 						LEFT JOIN ttrss_user_entries ON (ref_id = article_id AND
 							unread = true AND
-							ttrss_user_entries.owner_uid = '$owner_uid'),ttrss_feeds
-						WHERE ttrss_feeds.id = feed_id AND
-							hidden = false AND						
-							ttrss_labels2.owner_uid = '$owner_uid'
+							ttrss_user_entries.owner_uid = '$owner_uid')
+						LEFT JOIN ttrss_feeds ON (ttrss_feeds.id = feed_id AND hidden = false)
+						WHERE ttrss_labels2.owner_uid = '$owner_uid'
 					GROUP BY ttrss_labels2.id, caption");
 		
 			while ($line = db_fetch_assoc($result)) {
