b9ce0ba16908da7b24c8f35a971d6ac23445e0c7
Andrew Dolgov <fox@bah.spb.su>
2009-01-22 16:15:59 +0100

    view: lower counters rate limit

--- backend.php
+++ backend.php
@@ -240,7 +240,7 @@
 
 //			if (get_pref($link, "SYNC_COUNTERS") || ($mode == "prefetch" && $csync)) {
 
-			if (time() - $_SESSION["view:counters_stamp"] > 10) {
+			if (time() - $_SESSION["view:counters_stamp"] > 5) {
 				print "<counters>";
 				getAllCounters($link, $omode);
 				print "</counters>";
