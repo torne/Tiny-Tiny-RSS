c1a0541a9995c2ce45a2fbc6767a4680ba6e0b58
Andrew Dolgov <fox@bah.org.ru>
2009-02-07 15:12:03 +0300

    offline: do not sync hidden feeds

--- modules/backend-rpc.php
+++ modules/backend-rpc.php
@@ -586,7 +586,7 @@
 				print "<feeds>";
 
 				$result = db_query($link, "SELECT id, title, cat_id FROM
-					ttrss_feeds WHERE owner_uid = ".$_SESSION["uid"]);
+					ttrss_feeds WHERE hidden = false AND owner_uid = ".$_SESSION["uid"]);
 
 				while ($line = db_fetch_assoc($result)) {
 
@@ -679,11 +679,15 @@
 					}			
 
 					$result = db_query($link,
-						"SELECT DISTINCT id,title,guid,link,comments,
-								feed_id,content,updated,unread,marked FROM
-							ttrss_user_entries,ttrss_entries
-							WHERE $unread_qpart $cid_qpart $date_qpart
-							ref_id = id AND owner_uid = ".$_SESSION["uid"]."
+						"SELECT DISTINCT ttrss_entries.id,ttrss_entries.title,
+							guid,link,comments,
+							feed_id,content,updated,unread,marked FROM
+							ttrss_user_entries,ttrss_entries,ttrss_feeds
+						WHERE $unread_qpart $cid_qpart $date_qpart
+							hidden = false AND
+							ttrss_feeds.id = feed_id AND
+							ref_id = ttrss_entries.id AND 
+							ttrss_user_entries.owner_uid = ".$_SESSION["uid"]."
 							ORDER BY updated DESC LIMIT $limit OFFSET $skip");
 
 					if (function_exists('json_encode')) {
