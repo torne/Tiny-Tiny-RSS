b92fbcd8a52ef26a1700e0194e6a7a2ccb733b7d
Andrew Dolgov <fox@bah.org.ru>
2010-01-28 18:54:45 +0300

    only check for themes when there is a logged-in user

--- functions.php
+++ functions.php
@@ -2047,7 +2047,7 @@
 
 	function get_user_theme($link) {
 
-		if (get_schema_version($link) >= 63) {
+		if (get_schema_version($link) >= 63 && $_SESSION["uid"]) {
 			$theme_name = get_pref($link, "_THEME_ID");
 			if (is_dir("themes/$theme_name")) {
 				return $theme_name;
@@ -2062,7 +2062,7 @@
 
 	function get_user_theme_path($link) {
 
-		if (get_schema_version($link) >= 63) {
+		if (get_schema_version($link) >= 63 && $_SESSION["uid"]) {
 			$theme_name = get_pref($link, "_THEME_ID");
 
 			if ($theme_name && is_dir("themes/$theme_name")) {
