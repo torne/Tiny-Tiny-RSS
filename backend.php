c0793f64a801531486fde2c1fbce63ce3003701e
Andrew Dolgov <fox@bah.org.ru>
2010-09-04 11:59:33 +0400

    use better magic quotes removal fix

--- backend.php
+++ backend.php
@@ -3,11 +3,17 @@
 
 	/* remove ill effects of magic quotes */
 
-	if (get_magic_quotes_gpc()) {
-		$_REQUEST = array_map('stripslashes', $_REQUEST);
-		$_POST = array_map('stripslashes', $_POST);
-//		$_REQUEST = array_map('stripslashes', $_REQUEST);
-		$_COOKIE = array_map('stripslashes', $_COOKIE);
+	if (!get_magic_quotes_gpc()) {
+		function stripslashes_deep($value) {
+			$value = is_array($value) ? 
+				array_map('stripslashes_deep', $value) : stripslashes($value);
+				return $value;
+		}
+
+		$_POST = array_map('stripslashes_deep', $_POST);
+		$_GET = array_map('stripslashes_deep', $_GET);
+		$_COOKIE = array_map('stripslashes_deep', $_COOKIE);
+		$_REQUEST = array_map('stripslashes_deep', $_REQUEST);
 	}
 
 	require_once "sessions.php";
