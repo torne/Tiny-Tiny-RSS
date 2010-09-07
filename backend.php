5fa173729e21c4b0a222541a0f9e939711cf1989
Andrew Dolgov <fox@bah.org.ru>
2010-09-07 13:22:10 +0400

    make sure magic quote workaround actually works

--- backend.php
+++ backend.php
@@ -3,7 +3,7 @@
 
 	/* remove ill effects of magic quotes */
 
-	if (!get_magic_quotes_gpc()) {
+	if (get_magic_quotes_gpc()) {
 		function stripslashes_deep($value) {
 			$value = is_array($value) ? 
 				array_map('stripslashes_deep', $value) : stripslashes($value);
