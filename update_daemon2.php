7aabaa09e13482291194c3c8d86a0e8421ea009c
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2010-10-29 12:10:24 +0400

    daemon2: check proper lockfile location in reap_children()

--- update_daemon2.php
+++ update_daemon2.php
@@ -53,7 +53,8 @@
 
 		foreach ($children as $pid) {
 			if (pcntl_waitpid($pid, $status, WNOHANG) != $pid) {
-				if (file_is_locked(LOCK_DIRECTORY . "/update_daemon-$pid.lock")) {
+
+				if (file_is_locked("update_daemon-$pid.lock")) {
 					array_push($tmp, $pid);
 				} else {
 					_debug("[reap_children] child $pid seems active but lockfile is unlocked.");
