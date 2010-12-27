f52df691c3c8797a6a97914167c6b4873a273a28
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2010-12-27 12:40:46 +0300

    triple-pane: do not try to rework UI using javascript while in CDM

--- themes/triple-pane/theme.js
+++ themes/triple-pane/theme.js
@@ -1,14 +1,18 @@
 function themeBeforeLayout() {
-	$("headlines-wrap-inner").setAttribute("design", 'sidebar');
-	$("content-insert").setAttribute("region", "trailing");
-	$("content-insert").setStyle({
-		width: '50%',
-		height: 'auto'});
+	if ($("content-insert")) {
+		$("headlines-wrap-inner").setAttribute("design", 'sidebar');
+		$("content-insert").setAttribute("region", "trailing");
+		$("content-insert").setStyle({
+			width: '50%',
+			height: 'auto'});
+	}
 }
 
 function themeAfterLayout() {
-	$("headlines-toolbar").setStyle({
-		'border-right-width': '1px',
-		'border-color': '#88b0f0',
-		});
+	if ($("content-insert")) {
+		$("headlines-toolbar").setStyle({
+			'border-right-width': '1px',
+			'border-color': '#88b0f0',
+			});
+	}
 }
