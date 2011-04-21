6b8b3af87ee0f4cc123dab06a7c3ab405ab3df56
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2011-04-21 18:27:44 +0400

    filter_to_sql: properly set query limit by date

--- functions.php
+++ functions.php
@@ -7348,12 +7348,13 @@
 
 		if ($query) {
 			if (DB_TYPE == "pgsql") {
-				$query .= " AND ttrss_entries.date_entered > NOW() - INTERVAL '30 days'";
+				$query = " ($query) AND ttrss_entries.date_entered > NOW() - INTERVAL '14 days'";
 			} else {
-				$query .= " AND ttrss_entries.date_entered > DATE_SUB(NOW(), INTERVAL 30 DAY";
+				$query = " ($query) AND ttrss_entries.date_entered > DATE_SUB(NOW(), INTERVAL 14 DAY";
 			}
 			$query .= " AND ";
 		}
+
 
 		return $query;
 	}
