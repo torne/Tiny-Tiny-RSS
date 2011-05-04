6ff66966abe77d43a79b1e88333cf3582e1c4d16
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2011-05-04 23:04:21 +0400

    update it_IT translation

--- localized_schema.php
+++ localized_schema.php
@@ -1,4 +1,4 @@
-<?php # This file has been generated at:  Fri Apr 22 21:26:28 MSD 2011
+<?php # This file has been generated at:  Wed May 4 23:03:50 MSD 2011
 
 __("Title");
 __("Title or Content");
