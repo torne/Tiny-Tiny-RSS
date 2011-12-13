43dd8898bd8d82d526b6d56b9f6b2afd53af9162
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2011-12-13 13:18:34 +0400

    fix generator of localized_schema.php

--- utils/update-schema-translations.sh
+++ utils/update-schema-translations.sh
@@ -2,17 +2,18 @@
 
 BASENAME=`basename $0`
 TMPFILE="/tmp/$BASENAME-$$.tmp"
+OUTFILE="include/localized_schema.php"
 
 cat schema/ttrss_schema_pgsql.sql | grep 'insert.*pref_name' | awk -F\' '{ print $8 }' > $TMPFILE
 cat schema/ttrss_schema_pgsql.sql | grep 'insert.*pref_name' | awk -F\' '{ print $6 }' >> $TMPFILE
 
-echo "<?php # This file has been generated at: " `date` > localized_schema.php
-echo >> localized_schema.php
-cat utils/localized_schema.txt >> localized_schema.php
-echo >> localized_schema.php
+echo "<?php # This file has been generated at: " `date` > $OUTFILE
+echo >> $OUTFILE
+cat utils/localized_schema.txt >> $OUTFILE
+echo >> $OUTFILE
 
-cat $TMPFILE | grep -v '^$' | sed "s/.*/__('&');/" >> localized_schema.php
+cat $TMPFILE | grep -v '^$' | sed "s/.*/__('&');/" >> $OUTFILE
 
-echo "?>" >> localized_schema.php
+echo "?>" >> $OUTFILE
 
 rm $TMPFILE
