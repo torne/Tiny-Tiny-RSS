761501fc9fd619594670111a19ea00f4585a2c17
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2011-12-20 15:34:05 +0400

    release 1.5.8

--- include/version.php
+++ include/version.php
@@ -1,3 +1,3 @@
 <?php
-	define('VERSION', "1.5.7");
+	define('VERSION', "1.5.8");
 ?>
--- utils/create-release-deb.sh
+++ utils/create-release-deb.sh
@@ -14,7 +14,7 @@ debuild -i -us -uc -b -aamd64
 
 cd ..
 
-if [! -z "$DEPLOY_DEBS" ]; then
+if [ ! -z "$DEPLOY_DEBS" ]; then
 	reprepro -b /var/www/apt include unstable tt-rss*_i386.changes
 	reprepro -b /var/www/apt include unstable tt-rss*_amd64.changes
 fi
