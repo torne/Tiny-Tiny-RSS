9432bfa076738b47fce25c4a5a3293201bf21099
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2012-08-13 14:36:52 +0400

    add ttrss_feed_categories.parent_cat, bump schema

--- schema/ttrss_schema_pgsql.sql
+++ schema/ttrss_schema_pgsql.sql
@@ -52,6 +52,7 @@ create table ttrss_feed_categories(id serial not null primary key,
 	owner_uid integer not null references ttrss_users(id) on delete cascade,
 	collapsed boolean not null default false,
 	order_id integer not null default 0,
+	parent_cat integer references ttrss_feed_categories(id) on delete set null,
 	title varchar(200) not null);
 
 create table ttrss_feeds (id serial not null primary key,
@@ -231,7 +232,7 @@ create index ttrss_tags_post_int_id_idx on ttrss_tags(post_int_id);
 
 create table ttrss_version (schema_version int not null);
 
-insert into ttrss_version values (92);
+insert into ttrss_version values (93);
 
 create table ttrss_enclosures (id serial not null primary key,
 	content_url text not null,
