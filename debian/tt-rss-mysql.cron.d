8c25264dfd6aae0e3362823410cdfc3102a74782
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2012-10-19 14:38:54 +0400

    remove debian stuff

--- debian/tt-rss-mysql.cron.d
+++
@@ -1,4 +0,0 @@
-# /etc/cron.d/tt-rss-mysql: crontab fragment for tt-rss-mysql
-#  This update feeds for tiny tiny RSS every 20min 
-
-12,42 *     * * *     www-data	 /usr/bin/wget --output-document=/dev/null --quiet http://localhost/tt-rss/public.php?op=globalUpdateFeeds&daemon=1
--- debian/tt-rss-pgsql.cron.d
+++
@@ -1,4 +0,0 @@
-# /etc/cron.d/tt-rss-pgsql: crontab fragment for tt-rss-pgsql
-#  This update feeds for tiny tiny RSS every 20min 
-
-12,42 *     * * *     www-data  /usr/bin/wget --output-document=/dev/null --quiet http://localhost/tt-rss/public.php?op=globalUpdateFeeds&daemon=1
