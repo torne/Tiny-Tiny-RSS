4318a9b9924afb2f3b33b834b8ae292d17da9c25
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-02-20 16:13:02 +0400

    close_button: remove useless getInfo() method

--- plugins/close_button/init.php
+++ plugins/close_button/init.php
@@ -26,24 +26,5 @@ class Close_Button extends Plugin {
 
 		return $rv;
 	}
-
-	function getInfo() {
-		$id = db_escape_string($_REQUEST['id']);
-
-		$result = db_query($this->link, "SELECT title, link
-				FROM ttrss_entries, ttrss_user_entries
-				WHERE id = '$id' AND ref_id = id AND owner_uid = " .$_SESSION['uid']);
-
-		if (db_num_rows($result) != 0) {
-			$title = truncate_string(strip_tags(db_fetch_result($result, 0, 'title')),
-				100, '...');
-			$article_link = db_fetch_result($result, 0, 'link');
-		}
-
-		print json_encode(array("title" => $title, "link" => $article_link,
-				"id" => $id));
-	}
-
-
 }
 ?>
