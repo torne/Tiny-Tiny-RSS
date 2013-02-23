cd616c4c35a4910486a9903bbfd95f1e9b9f3463
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-02-23 21:28:09 +0400

    nsfw: make tags list configurable

--- plugins/nsfw/init.php
+++ plugins/nsfw/init.php
@@ -6,7 +6,7 @@ class NSFW extends Plugin {
 
 	function about() {
 		return array(1.0,
-			"Hide article content if tags contain \"nsfw\"",
+			"Hide article content based on tags",
 			"fox",
 			false);
 	}
@@ -17,6 +17,7 @@ class NSFW extends Plugin {
 
 		$host->add_hook($host::HOOK_RENDER_ARTICLE, $this);
 		$host->add_hook($host::HOOK_RENDER_ARTICLE_CDM, $this);
+		$host->add_hook($host::HOOK_PREFS_TAB, $this);
 
 	}
 
@@ -25,8 +26,9 @@ class NSFW extends Plugin {
 	}
 
 	function hook_render_article($article) {
+		$tags = array_map("trim", explode(", ", $this->host->get($this, "tags")));
 
-		if (array_search("nsfw", $article["tags"]) !== FALSE) {
+		if (count(array_intersect($tags, $article["tags"])) > 0) {
 			$article["content"] = "<div class='nswf wrapper'><button onclick=\"nsfwShow(this)\">".__("Not work safe (click to toggle)")."</button>
 				<div class='nswf content' style='display : none'>".$article["content"]."</div></div>";
 		}
@@ -35,12 +37,68 @@ class NSFW extends Plugin {
 	}
 
 	function hook_render_article_cdm($article) {
-		if (array_search("nsfw", $article["tags"]) !== FALSE) {
+		$tags = array_map("trim", explode(", ", $this->host->get($this, "tags")));
+
+		if (count(array_intersect($tags, $article["tags"])) > 0) {
 			$article["content"] = "<div class='nswf wrapper'><button onclick=\"nsfwShow(this)\">".__("Not work safe (click to toggle)")."</button>
 				<div class='nswf content' style='display : none'>".$article["content"]."</div></div>";
 		}
 
 		return $article;
+	}
+
+	function hook_prefs_tab($args) {
+		if ($args != "prefPrefs") return;
+
+		print "<div dojoType=\"dijit.layout.AccordionPane\" title=\"".__("NSFW Plugin")."\">";
+
+		print "<br/>";
+
+		$tags = $this->host->get($this, "tags");
+
+		print "<form dojoType=\"dijit.form.Form\">";
+
+		print "<script type=\"dojo/method\" event=\"onSubmit\" args=\"evt\">
+			evt.preventDefault();
+			if (this.validate()) {
+				new Ajax.Request('backend.php', {
+					parameters: dojo.objectToQuery(this.getValues()),
+					onComplete: function(transport) {
+						notify_info(transport.responseText);
+					}
+				});
+				//this.reset();
+			}
+			</script>";
+
+			print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"op\" value=\"pluginhandler\">";
+			print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"method\" value=\"save\">";
+			print "<input dojoType=\"dijit.form.TextBox\" style=\"display : none\" name=\"plugin\" value=\"nsfw\">";
+
+			print "<table width=\"100%\" class=\"prefPrefsList\">";
+
+			print "<tr><td width=\"40%\">".__("Tags to consider NSFW (comma-separated)")."</td>";
+			print "<td class=\"prefValue\"><input dojoType=\"dijit.form.ValidationTextBox\" required=\"1\" name=\"tags\" value=\"$tags\"></td></tr>";
+
+			print "</table>";
+
+			print "<p><button dojoType=\"dijit.form.Button\" type=\"submit\">".
+				__("Save")."</button>";
+
+			print "</form>";
+
+			print "</div>"; #pane
+	}
+
+	function save() {
+		$tags = explode(",", db_escape_string($_POST["tags"]));
+		$tags = array_map("trim", $tags);
+		$tags = array_map("mb_strtolower", $tags);
+		$tags = join(", ", $tags);
+
+		$this->host->set($this, "tags", $tags);
+
+		echo __("Configuration saved.");
 	}
 
 }
