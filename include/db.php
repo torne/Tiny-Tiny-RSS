8fe3a493d42fd5f7a781e8bb46d944358a5d029b
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-02-27 15:48:33 +0400

    db: replace QUERY_LOG with _DEBUG_QUERY_LOG

--- include/db.php
+++ include/db.php
@@ -52,7 +52,7 @@ function db_escape_string($s, $strip_tags = true) {
 }
 
 function db_query($link, $query, $die_on_error = true) {
-	if ($_REQUEST["qlog"] || defined('QUERY_LOG')) {
+	if ($_REQUEST["qlog"] || defined('_DEBUG_QUERY_LOG')) {
 		$query = trim($query);
 		error_log($_SESSION["uid"] . ":" . $_REQUEST["op"] . "/" . $_REQUEST["method"] .
 				" $query\n", 3, "/tmp/ttrss-query.log");
