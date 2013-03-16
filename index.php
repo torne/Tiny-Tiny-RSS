1b113281ffa3c0fad96b139df39de566567b8d9d
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-03-16 12:06:34 +0400

    make version checks a bit more readable

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 	// we need a separate check here because functions.php might get parsed
 	// incorrectly before 5.3 because of :: syntax.
-	if (!(version_compare(PHP_VERSION, '5.3.0', '>='))) {
+	if (version_compare(PHP_VERSION, '5.3.0', '<')) {
 		print "<b>Fatal Error</b>: PHP version 5.3.0 or newer required.\n";
 		exit;
 	}
