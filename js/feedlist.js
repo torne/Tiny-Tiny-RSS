d5a8e864175bb941cb1e3d72a8b653a904cbdbe2
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-03-23 09:30:57 +0400

    show loading indicator when appending headlines

--- js/feedlist.js
+++ js/feedlist.js
@@ -125,11 +125,9 @@ function viewfeed(feed, method, is_cat, offset, background, infscroll_req) {
 
 			Form.enable("main_toolbar_form");
 
-			if (!offset) {
-				if (!setFeedExpandoIcon(feed, is_cat,
-							(is_cat) ? 'images/indicator_tiny.gif' : 'images/indicator_white.gif'))
+			if (!setFeedExpandoIcon(feed, is_cat,
+				(is_cat) ? 'images/indicator_tiny.gif' : 'images/indicator_white.gif'))
 					notify_progress("Loading, please wait...", true);
-			}
 		}
 
 		query += "&cat=" + is_cat;
