d98de737f53cdb774408997a594580b4f6e00343
kythyria <kythyria@berigora.net>
2013-03-25 11:16:35 +0000

    Make the Penny Arcade plugin pull in news posts as well as comics

--- plugins/af_pennyarcade/init.php
+++ plugins/af_pennyarcade/init.php
@@ -5,7 +5,7 @@ class Af_PennyArcade extends Plugin {
 	private $host;
 
 	function about() {
-		return array(1.0,
+		return array(1.1,
 			"Strip unnecessary stuff from PA feeds",
 			"fox");
 	}
@@ -22,28 +22,58 @@ class Af_PennyArcade extends Plugin {
 
 		if (strpos($article["link"], "penny-arcade.com") !== FALSE && strpos($article["title"], "Comic:") !== FALSE) {
 			if (strpos($article["plugin_data"], "pennyarcade,$owner_uid:") === FALSE) {
-
+				
+				if ($debug_enabled) {
+					_debug("af_pennyarcade: Processing comic");
+				}
+				
 				$doc = new DOMDocument();
-				@$doc->loadHTML(fetch_file_contents($article["link"]));
+				$doc->loadHTML(fetch_file_contents($article["link"]));
 
 				$basenode = false;
 
 				if ($doc) {
 					$xpath = new DOMXPath($doc);
-					$entries = $xpath->query('(//img[@src])'); // we might also check for img[@class='strip'] I guess...
-
-					$matches = array();
+					$entries = $xpath->query('(//div[@class="post comic"])');
 
 					foreach ($entries as $entry) {
-
-						if (preg_match("/(http:\/\/art.penny-arcade.com\/.*)/i", $entry->getAttribute("src"), $matches)) {
-
-							$basenode = $entry;
-							break;
-						}
+						$basenode = $entry;
 					}
 
 					if ($basenode) {
+						$article["content"] = $doc->saveXML($basenode);
+						$article["plugin_data"] = "pennyarcade,$owner_uid:" . $article["plugin_data"];
+					}
+				}
+			} else if (isset($article["stored"]["content"])) {
+				$article["content"] = $article["stored"]["content"];
+			}
+		}
+	
+		if (strpos($article["link"], "penny-arcade.com") !== FALSE && strpos($article["title"], "News Post:") !== FALSE) {
+			if (strpos($article["plugin_data"], "pennyarcade,$owner_uid:") === FALSE) {
+				if ($debug_enabled) {
+					_debug("af_pennyarcade: Processing news post");
+				}
+				$doc = new DOMDocument();
+				$doc->loadHTML(fetch_file_contents($article["link"]));
+				
+				if ($doc) {
+					$xpath = new DOMXPath($doc);
+					$entries = $xpath->query('(//div[@class="post"])');
+					
+					$basenode = false;
+					
+					foreach ($entries as $entry) {
+						$basenode = $entry;
+					}
+					
+					$uninteresting = $xpath->query('(//div[@class="heading"])');
+					foreach ($uninteresting as $i) {
+						$i->parentNode->removeChild($i);
+					}
+					
+					if ($basenode){
 						$article["content"] = $doc->saveXML($basenode);
 						$article["plugin_data"] = "pennyarcade,$owner_uid:" . $article["plugin_data"];
 					}
