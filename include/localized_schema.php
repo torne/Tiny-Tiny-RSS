27f018bab7718fa2675d35430d98526eecae52d2
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-03-27 16:48:25 +0400

    rebase translations

--- include/localized_schema.php
+++ include/localized_schema.php
@@ -1,4 +1,4 @@
-<?php # This file has been generated at:  Wed Mar 27 12:48:18 MSK 2013
+<?php # This file has been generated at:  Wed Mar 27 16:48:20 MSK 2013
 
 __("Title");
 __("Title or Content");
