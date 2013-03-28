2137d67496232fb6c373598c2f046e24fc4adb82
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-03-28 12:40:56 +0400

    sessions: properly check for cookie being set

--- include/sessions.php
+++ include/sessions.php
@@ -103,14 +103,12 @@
 	}
 
 	if (!defined('TTRSS_SESSION_NAME') || TTRSS_SESSION_NAME != 'ttrss_api_sid') {
-		if ($_COOKIE[$session_name]) {
+		if (isset($_COOKIE[$session_name])) {
 			@session_start();
 
 			if (!$_SESSION["uid"]) {
 				session_destroy();
-				if (isset($_COOKIE[session_name()])) {
-				   setcookie(session_name(), '', time()-42000, '/');
-				}
+			   setcookie(session_name(), '', time()-42000, '/');
 			}
 		}
 	}
