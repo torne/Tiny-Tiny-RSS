196464eef212a4e0f6be392cea2128a7143f0f80
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-04-07 00:56:48 +0400

    installer: remove check for pubsubhubbub which is never defined

--- install/index.php
+++ install/index.php
@@ -57,10 +57,6 @@
 			array_push($errors, "PHP safe mode setting is not supported.");
 		} */
 
-		if ((PUBSUBHUBBUB_HUB || PUBSUBHUBBUB_ENABLED) && !function_exists("curl_init")) {
-			array_push($errors, "PHP support for CURL is required for PubSubHubbub.");
-		}
-
 		if (!class_exists("DOMDocument")) {
 			array_push($errors, "PHP support for DOMDocument is required, but was not found.");
 		}
