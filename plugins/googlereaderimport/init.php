c6c968b042f4853e3a069bb75be96bd919630275
Florian Schlichting <fschlich@zedat.fu-berlin.de>
2013-04-21 13:20:50 +0200

    googlereaderimport: actually pass $link to create_article()
    otherwise the link db field will be empty, and viewing the original
    article of an imported starred item results in an empty backend.php page

--- plugins/googlereaderimport/init.php
+++ plugins/googlereaderimport/init.php
@@ -164,7 +164,7 @@ class GoogleReaderImport extends Plugin {
 					$processed++;
 
 					$imported += (int) $this->create_article($owner_uid, $guid, $title,
-						$updated,  $content, $author, $sql_set_marked, $tags,
+						$link, $updated,  $content, $author, $sql_set_marked, $tags,
 						$orig_feed_data);
 
 					if ($file && $processed % 25 == 0) {
@@ -196,7 +196,7 @@ class GoogleReaderImport extends Plugin {
 	}
 
 	// expects ESCAPED data
-	private function create_article($owner_uid, $guid, $title, $updated,  $content, $author, $marked, $tags, $orig_feed_data) {
+	private function create_article($owner_uid, $guid, $title, $link, $updated,  $content, $author, $marked, $tags, $orig_feed_data) {
 
 		if (!$guid) $guid = sha1($link);
 
