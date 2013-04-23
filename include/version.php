6316898f8de2c5163fef32105427416cedb13fee
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-04-23 20:22:55 +0400

    special version reporting for git

--- include/version.php
+++ include/version.php
@@ -1,3 +1,18 @@
 <?php
-	define('VERSION', "1.7.8");
+	define('VERSION_STATIC', '1.7.8');
+
+	function get_version() {
+		$root_dir = dirname(dirname(__FILE__));
+
+		if (is_dir("$root_dir/.git") && file_exists("$root_dir/.git/ORIG_HEAD")) {
+
+			$suffix = date("Ymd", filemtime("$root_dir/.git/ORIG_HEAD"));
+
+			return VERSION_STATIC . ".$suffix";
+		} else {
+			return VERSION_STATIC;
+		}
+	}
+
+	define('VERSION', get_version());
 ?>
