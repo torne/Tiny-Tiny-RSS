50d4b30b338bae62fd3eb8692ba6b9b6e8e53f26
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-04-24 08:56:37 +0400

    get_version: force UTC

--- include/version.php
+++ include/version.php
@@ -2,6 +2,7 @@
 	define('VERSION_STATIC', '1.7.8');
 
 	function get_version() {
+		date_default_timezone_set('UTC');
 		$root_dir = dirname(dirname(__FILE__));
 
 		if (is_dir("$root_dir/.git") && file_exists("$root_dir/.git/ORIG_HEAD")) {
