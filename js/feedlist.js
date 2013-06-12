f6ba763d6bd0380cadf32d7c39854115f7cc3eda
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-06-12 14:04:31 +0400

    Revert "Load selected feed after a short timeout"
    This reverts commit 17c5d44cf42f6c99786efbc45b788d910fb4c4be.

--- js/feedlist.js
+++ js/feedlist.js
@@ -2,7 +2,6 @@ var _infscroll_disable = 0;
 var _infscroll_request_sent = 0;
 var _search_query = false;
 var _viewfeed_last = 0;
-var _viewfeed_timeout = false;
 
 var counters_last_request = 0;
 
@@ -133,21 +132,15 @@ function viewfeed(feed, method, is_cat, offset, background, infscroll_req) {
 
 		console.log(query);
 
-		if (_viewfeed_timeout) {
-			setFeedExpandoIcon(getActiveFeedId(), activeFeedIsCat(), 'images/blank_icon.gif');
-			clearTimeout(_viewfeed_timeout);
-		}
 		setActiveFeedId(feed, is_cat);
 
-		_viewfeed_timeout = setTimeout(function()) {
 		new Ajax.Request("backend.php", {
-				parameters: query,
-				onComplete: function(transport) {
-					setFeedExpandoIcon(feed, is_cat, 'images/blank_icon.gif');
-					headlines_callback2(transport, offset, background, infscroll_req);
-					PluginHost.run(PluginHost.HOOK_FEED_LOADED, [feed, is_cat]);
-				} });
-		}, 250); // 250ms delay
+			parameters: query,
+			onComplete: function(transport) {
+				setFeedExpandoIcon(feed, is_cat, 'images/blank_icon.gif');
+				headlines_callback2(transport, offset, background, infscroll_req);
+				PluginHost.run(PluginHost.HOOK_FEED_LOADED, [feed, is_cat]);
+			} });
 
 	} catch (e) {
 		exception_error("viewfeed", e);
