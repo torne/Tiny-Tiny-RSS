4f00f55ca2ecd2e5a75c2c4ef37ca0e1143a7ac7
Andrew Dolgov <fox@madoka.volgo-balt.ru>
2013-07-11 15:40:09 +0400

    parser: add charset recoding hack for systems where libxml is build without support for iconv (handles libxml error 32)

--- classes/feedparser.php
+++ classes/feedparser.php
@@ -20,6 +20,24 @@ class FeedParser {
 
 		$error = libxml_get_last_error();
 
+		// libxml compiled without iconv?
+		if ($error && $error->code == 32) {
+			if (preg_match('/^(<\\?xml .*?)encoding="(.+?)"(.*?\\?>)/', $data, $matches) === 1) {
+				libxml_clear_errors();
+
+				$enc = $matches[2];
+
+	         $data = iconv($enc, 'UTF-8//IGNORE', $data);
+	         $data = preg_replace('/^<\\?xml .*?\\?>/', $matches[1] . $matches[3] , $data);
+
+				$this->doc = new DOMDocument();
+				$this->doc->loadXML($data);
+
+				$error = libxml_get_last_error();
+		   }
+		}
+
+		// some terrible invalid unicode entity?
 		if ($error && $error->code == 9) {
 			libxml_clear_errors();
 
