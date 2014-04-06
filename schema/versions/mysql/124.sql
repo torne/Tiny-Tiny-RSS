44a45a635143ade59f24fd5058a163e3a869af41
Anders Larsson <andla@thethundertemplars.com>
2014-04-06 15:08:43 +0200

    Fix mysql update 124 data type for resetpass_token
    Currently mysql schema update 124 sets resetpass_token type to boolean
    instead of VARCHAR(250). This prevents reset password functionality from
    working correctly.

--- schema/versions/mysql/124.sql
+++ schema/versions/mysql/124.sql
@@ -1,6 +1,6 @@
 begin;
 
-alter table ttrss_users add column resetpass_token boolean;
+alter table ttrss_users add column resetpass_token varchar(250);
 alter table ttrss_users alter column resetpass_token set default null;
 
 update ttrss_version set schema_version = 124;
