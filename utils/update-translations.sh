f8eb8d78acef8beead4882cd46c2a36a3250809f
Andrew Dolgov <noreply@fakecake.org>
2014-09-06 15:11:06 +0400

    use Perl instead of Java as a base language for JS files; rebase translations

--- utils/update-translations.sh
+++ utils/update-translations.sh
@@ -3,7 +3,7 @@ TEMPLATE=messages.pot
 
 xgettext -kT_js_decl -kT_sprintf -k_ngettext:1,2 -kT_ngettext:1,2 -k__ -L PHP -o $TEMPLATE *.php include/*.php `find classes -iname '*.php'` `find plugins -iname '*.php'`
 
-xgettext --from-code utf-8 -k__ -knotify_info -knotify_progress -kngettext -L Java -j -o $TEMPLATE js/*.js `find plugins -iname '*.js'`
+xgettext --from-code utf-8 -k__ -knotify_info -knotify_progress -kngettext -L Perl -j -o $TEMPLATE js/*.js `find plugins -iname '*.js'`
 
 update_lang() {
 	if [ -f $1.po ]; then
