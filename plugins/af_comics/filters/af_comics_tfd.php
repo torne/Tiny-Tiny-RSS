5613bb3584356e51e5e476484d6e8b38210707d4
Andrew Dolgov <noreply@fakecake.org>
2015-06-18 08:28:54 +0300

    fix toothpastefordinner valiant efforts to prevent scraping

--- plugins/af_comics/filters/af_comics_tfd.php
+++ plugins/af_comics/filters/af_comics_tfd.php
@@ -6,12 +6,15 @@ class Af_Comics_Tfd extends Af_ComicFilter {
 	}
 
 	function process(&$article) {
-		$owner_uid = $article["owner_uid"];
-
 		if (strpos($article["link"], "toothpastefordinner.com") !== FALSE) {
-			$doc = new DOMDocument();
+			$res = fetch_file_contents($article["link"], false, false, false,
+				false, false, 0,
+				"Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)");
 
-			@$doc->loadHTML(fetch_file_contents($article["link"]));
+			if (!$res) return $article;
+
+			$doc = new DOMDocument();
+			$doc->loadHTML($res);
 
 			$basenode = false;
 
